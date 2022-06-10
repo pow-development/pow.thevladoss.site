ce09c3972eef952674636b1068df37f07359976d
Stanislav <stasdamaratskiy@gmail.com>
2022-06-11 02:15:50 +0300

    add menu temaplate

---
+++ menu/navbar.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Navbar</title>
+
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="../styles/bootstrap.min.css">
+  <script src="../js/bootstrap.bundle.js"></script>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">POW</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="nolognavbar.php">Главная</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Магазин</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Лига</a>
+              </li>
+            </ul>
+        <!-- <div class="row mx-auto">
+          <div class="col"> <a class="nav-link active" aria-current="page" href="#">Главная</a></div>
+          <div class="col"><a class="nav-link active" aria-current="page" href="#">Магазин</a></div>
+          <div class="col"> <a class="nav-link active" aria-current="page" href="#">Лига</a></div>
+        </div> -->
+
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Поиск..." aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Найти</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+</body>
+
+</html>
