6f7c94412b68c86fb0196aa31143f028f6f6a997
Stanislav <stasdamaratskiy@gmail.com>
2022-06-11 12:57:53 +0300

    nav

--- menu/nolognavbar.php
+++ menu/nolognavbar.php
@@ -10,37 +10,23 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="../styles/bootstrap.min.css">
     <script src="../js/bootstrap.bundle.js"></script>
+
+    <!--CSS-->
+    <link rel="stylesheet" href="../styles/custom/nav.css">
+
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav">
         <div class="container">
-            <a class="navbar-brand" href="#">POW</a>
+            <a class="navbar-brand" href="#">POWВолонтерам</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="nolognavbar.php">Главная</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Магазин</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Лига</a>
-                    </li>
-                </ul>
-                <!-- <div class="row mx-auto">
-          <div class="col"> <a class="nav-link active" aria-current="page" href="#">Главная</a></div>
-          <div class="col"><a class="nav-link active" aria-current="page" href="#">Магазин</a></div>
-          <div class="col"> <a class="nav-link active" aria-current="page" href="#">Лига</a></div>
-        </div> -->
-
-                <div>
-                    <button class="btn">Войти</button>
-                    <button class="btn">Регистрация</button>
+            <div class="collapse navbar-collapse row justify-content-end" id="navbarSupportedContent">
+                <div class="col-3 align-self-end" >
+                    <button class="btn"><img src="../img/log-in.png" width="15px" height="15px" class="icon"> Вход</button>
+                    <button class="btn"><img src="../img/user-plus.png" width="15px" height="15px" class="icon"> Регистрация</button>
                 </div>
             </div>
         </div>
